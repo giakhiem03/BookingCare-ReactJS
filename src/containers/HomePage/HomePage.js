8da16f7b2cec577afc8fcc337a1eb8eb20347109
GiaKhiem <giakhiem271@gmail.com>
2025-02-28 14:45:44 +0700

    using React-Slider-Carousel

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import Header from "./Header";
+import Specialty from "./Section/Specialty";
 
 class HomePage extends Component {
     render() {
         return (
-            <div className="">
+            <div>
                 <Header />
+                <Specialty />
             </div>
         );
     }
---
+++ src/containers/HomePage/Section/Specialty.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import "./Specialty.scss";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import specialtyImg from "../../../assets/specialty/cosuongkhop.jpg";
+
+class Specialty extends Component {
+    render() {
+        let settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 4,
+            slidesToScroll: 2,
+        };
+        return (
+            <div className="section-specialty">
+                <div className="specialty-container">
+                    <div className="specialty-header">
+                        <span className="title-section">
+                            Chuyên khoa phổ biến
+                        </span>
+                        <button className="btn-section">Xem thêm</button>
+                    </div>
+                    <div className="specialty-body">
+                        <Slider {...settings}>
+                            <div className="specialty-customize">
+                                <div className="bg-image"></div>
+                                <div>Cơ xương khớp 1</div>
+                            </div>
+                            <div className="specialty-customize">
+                                <div className="bg-image"></div>
+                                <div>Cơ xương khớp 1</div>
+                            </div>
+                            <div className="specialty-customize">
+                                <div className="bg-image"></div>
+                                <div>Cơ xương khớp 1</div>
+                            </div>
+                            <div className="specialty-customize">
+                                <div className="bg-image"></div>
+                                <div>Cơ xương khớp 1</div>
+                            </div>
+                            <div className="specialty-customize">
+                                <div className="bg-image"></div>
+                                <div>Cơ xương khớp 1</div>
+                            </div>
+                            <div className="specialty-customize">
+                                <div className="bg-image"></div>
+                                <div>Cơ xương khớp 1</div>
+                            </div>
+                            <div className="specialty-customize">
+                                <div className="bg-image"></div>
+                                <div>Cơ xương khớp 1</div>
+                            </div>
+                        </Slider>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        isLoggedIn: state.user.isLoggedIn,
+        language: state.app.language,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Specialty);
